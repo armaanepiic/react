ab7a753e8c9481392cfde951849b54ab3f7ab256
arman <armaanepiic@gmail.com>
2025-12-03 13:26:11 +0600

    group related state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [inputs, setInputs] = useState([
+    {
+      id: 1,
+      label: "input",
+    },
+  ]);
+  function handleAddInput() {
+    const nextId = inputs[inputs.length - 1].id;
+    setInputs(
+      //   inputs.concat({
+      //     id: nextId,
+      //     label: "input",
+      //   })
+      [
+        ...inputs,
+        {
+          id: nextId,
+          label: "input",
+        },
+      ]
+    );
+  }
   return (
     <div>
-      <div style={{ marginTop: "20px" }}>
+      {inputs.map((input) => (
+        <div key={input.id} style={{ marginBottom: "5px" }}>
+          <input type="text" label={input.label} />
+        </div>
+      ))}
+      <div style={{ marginTop: "20px" }} onClick={handleAddInput}>
         <button>Add Input</button>
       </div>
     </div>
