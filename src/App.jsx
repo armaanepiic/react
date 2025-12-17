2f75b7176b33be20cfb4c3eb55aaa02ca9f93fa0
arman <armaanepiic@gmail.com>
2025-12-17 18:54:43 +0600

    Preventing default behavior of form

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-function Button({ onSmash, children }) {
-  return (
-    <button
-      onClick={(e) => {
-        e.stopPropagation();
-        // console.dir(e);
-        onSmash();
-      }}
-    >{children}
-    </button>
-  );
-}
-
 export default function App() {
   return (
-    <div
-      onClick={() => {
-        console.log("You clicked on the toolbar!");
-      }}
-    >
-      <Button onSmash={() => console.log("Playing!")}>Play Movie</Button>
-      <Button onSmash={() => console.log("Uploading!")}>Upload Image</Button>
-    </div>
+    <form onSubmit={(e) => {
+      e.preventDefault();
+    }}>
+      <input />
+      <button>Send</button>
+    </form>
   );
 }
